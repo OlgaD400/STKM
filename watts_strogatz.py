83eb57c46d36898ed03f676df9c5ca0673341272
Olga Dorabiala <Olga400916@gmail.com>
2023-08-23 13:24:57 -0700

    refactor code

--- watts_strogatz.py
+++ watts_strogatz.py
@@ -1,51 +1,58 @@
-import networkx as nx 
-import random 
+""" Create Watts Strogatz Dynamic Graph """
+import random
+import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
-
 
 class WattsStrogatz:
-    def __init__(self, n,q, probability):
-        self.g = nx.Graph()
-        self.n = n
-        self.q = q
+    """ Watts Strogatz Dynamic Graph """
+    def __init__(self, num_nodes: int ,num_neighbors: int, probability: float):
+        """ Initialize the Watts Strogatz dynamic graph.
+        
+        Args:
+            num_nodes: number of nodes in graph
+            num_neighbors: number of neighbors each node in the graph has 
+            probability: probability of an edge being moved at each time step
+        """
+        self.graph = nx.Graph()
+        self.num_nodes = num_nodes
+        self.num_neighbors = num_neighbors
         self.probability = probability
 
-        for i in range(self.n):
-            for j in range(1, int(self.q/2 + 1)):
-                self.g.add_edge(i, (i+j)%self.n)
-                self.g.add_edge(i, (i-j)%self.n)
-        
-        self.pos = nx.spring_layout(self.g)
+        for i in range(self.num_nodes):
+            for j in range(1, int(self.num_neighbors/2 + 1)):
+                self.graph.add_edge(i, (i+j)%self.num_nodes)
+                self.graph.add_edge(i, (i-j)%self.num_nodes)
+        self.pos = nx.spring_layout(self.graph)
 
 
     def visualize(self):
+        """ Visualize one time step of the dyanmic graph."""
         plt.cla()
-        nx.draw(self.g, pos = self.pos, with_labels = True)
+        nx.draw(self.graph, pos = self.pos, with_labels = True)
 
     def update(self):
-        edgs = self.g.edges()
+        """ Update graph by randomly moving edges. """
+        edgs = self.graph.edges()
 
-        for u,v in edgs:
-            nds = list(self.g.nodes())
+        for node_1, node_2 in edgs:
+            nds = list(self.graph.nodes())
 
             if random.random() <= self.probability:
-                self.g.remove_edge(u, v)
-                
+                self.graph.remove_edge(node_1, node_2)
                 #ensure no self-connection
-                nds.remove(u)
+                nds.remove(node_1)
                 #ensure you don't choose an existing connection
-                for j in self.g.neighbors(u):
+                for j in self.graph.neighbors(node_1):
                     try:
                         nds.remove(j)
                     except:
                         continue
 
-                #choose a new node 
+                #choose a new node
                 new_node = random.choice(nds)
-                self.g.add_edge(u, new_node)
+                self.graph.add_edge(node_1, new_node)
 
-        return nx.adjacency_matrix(self.g, nodelist = range(self.n))
+        return nx.adjacency_matrix(self.graph, nodelist = range(self.num_nodes))
 
 # WS = WattsStrogatz(n = 8, q = 2, probability = .20)
 
@@ -54,5 +61,3 @@ class WattsStrogatz:
 #     WS.visualize()
 #     plt.show()
     # print(np.sum(connectivity, axis = 1))
-
-
