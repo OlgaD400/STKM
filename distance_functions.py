536deaada6ba5f39279e1805501bcf6e21139ff6
Olga Dorabiala <Olga400916@gmail.com>
2023-08-23 13:38:54 -0700

    refactor code

--- distance_functions.py
+++ distance_functions.py
@@ -1,34 +1,41 @@
+""" Graph distance functions. """
 import numpy as np
 
-def temporal_graph_distance(connectivity_matrix):
+def s_journey(connectivity_matrix: np.ndarray):
+    """ Calculate the s-journey distance between every pair of nodes at every time step.
+    
+    Args:
+        connectivity_matrix: Connectivity matrix of dynamic graph
+    """
     distance_matrix = np.copy(connectivity_matrix)
-    t,n,n = distance_matrix.shape
+    timesteps, num_nodes, _ = distance_matrix.shape
 
     #Replace all off-diagonal zeros with infinity
-    diagonal_mask = np.array([np.eye(n, dtype = bool)]*t)
+    diagonal_mask = np.array([np.eye(num_nodes, dtype = bool)]*timesteps)
     distance_matrix = np.where((distance_matrix == 0)&(~diagonal_mask), np.inf, distance_matrix)
 
     #iterate backwards through each time slice
-    for time in np.arange(t-1)[::-1]:
-        slice = distance_matrix[time, :,:]
+    for time in np.arange(timesteps-1)[::-1]:
+        current_slice = distance_matrix[time, :,:]
         next_slice = distance_matrix[time+1,:,:]
 
         #iterate through each row
-        for row in range(n):
-            connections = np.where(slice[row] == 1)[0]
-            ind_to_update = np.where(slice[row]>1)[0]
+        for row in range(num_nodes):
+            connections = np.where(current_slice[row] == 1)[0]
+            ind_to_update = np.where(current_slice[row]>1)[0]
 
             #If there are no connections, there's nothing to update
             #If there are no np.infs, there's nothing to update
             if (len(connections)>0) & (len(ind_to_update)>0):
                 if len(connections)>1:
-                    connections_distance = np.min(next_slice[connections,:][:,ind_to_update], axis = 0) + 1
+                    connections_distance = np.min(next_slice[connections,:][:,ind_to_update],
+                                                  axis = 0) + 1
                 else:
                     connections_distance = next_slice[connections,:][:,ind_to_update] + 1
-                slice[row, ind_to_update] = np.minimum(slice[row, ind_to_update], connections_distance)
-            else: 
+                current_slice[row, ind_to_update] = np.minimum(current_slice[row, ind_to_update],
+                                                               connections_distance)
+            else:
                 continue
-    
     #Replace any nonzero entries on diagonals with zero
     distance_matrix = np.where(diagonal_mask, 0, distance_matrix)
     return distance_matrix
