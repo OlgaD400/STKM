86bcfa1224beab72d471802b17c7e83e97370185
Olga Dorabiala <Olga400916@gmail.com>
2023-09-15 16:28:55 -0400

    add folders

---
+++ stgkm/distance_functions.py
@@ -0,0 +1,41 @@
+""" Graph distance functions. """
+import numpy as np
+
+def s_journey(connectivity_matrix: np.ndarray):
+    """ Calculate the s-journey distance between every pair of nodes at every time step.
+    
+    Args:
+        connectivity_matrix: Connectivity matrix of dynamic graph
+    """
+    distance_matrix = np.copy(connectivity_matrix)
+    timesteps, num_nodes, _ = distance_matrix.shape
+
+    #Replace all off-diagonal zeros with infinity
+    diagonal_mask = np.array([np.eye(num_nodes, dtype = bool)]*timesteps)
+    distance_matrix = np.where((distance_matrix == 0)&(~diagonal_mask), np.inf, distance_matrix)
+
+    #iterate backwards through each time slice
+    for time in np.arange(timesteps-1)[::-1]:
+        current_slice = distance_matrix[time, :,:]
+        next_slice = distance_matrix[time+1,:,:]
+
+        #iterate through each row
+        for row in range(num_nodes):
+            connections = np.where(current_slice[row] == 1)[0]
+            ind_to_update = np.where(current_slice[row]>1)[0]
+
+            #If there are no connections, there's nothing to update
+            #If there are no np.infs, there's nothing to update
+            if (len(connections)>0) & (len(ind_to_update)>0):
+                if len(connections)>1:
+                    connections_distance = np.min(next_slice[connections,:][:,ind_to_update],
+                                                  axis = 0) + 1
+                else:
+                    connections_distance = next_slice[connections,:][:,ind_to_update] + 1
+                current_slice[row, ind_to_update] = np.minimum(current_slice[row, ind_to_update],
+                                                               connections_distance)
+            else:
+                continue
+    #Replace any nonzero entries on diagonals with zero
+    distance_matrix = np.where(diagonal_mask, 0, distance_matrix)
+    return distance_matrix
