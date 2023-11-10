5ef350836ae762becbda7d3c8c1a07547c9ecae9
Olga Dorabiala <Olga400916@gmail.com>
2023-11-10 09:22:24 +0000

    clean up repo

--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,7 @@ from stkm.STKM import STKM, simplex_prox
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 class Tests(unittest.TestCase):
     def test_simplex(self):
         """
@@ -15,10 +16,11 @@ class Tests(unittest.TestCase):
             proj = simplex_prox(a, num)
 
         assert np.all(np.isclose(np.sum(proj, axis=2), num))
-    
+
     def run_tests(self):
         """Run all tests"""
         self.test_simplex()
+
 
 tests = Tests()
 tests.run_tests()
