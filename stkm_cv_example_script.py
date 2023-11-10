58dab4445ee0f7bd983927ba0ac2dbb444f3633f
Olga Dorabiala <Olga400916@gmail.com>
2023-11-10 09:39:53 +0000

    updated example script

--- stkm_cv_example_script.py
+++ stkm_cv_example_script.py
@@ -1,37 +1,36 @@
-
+"""Example script for running stkm for cv problems."""
 from stkm.STKM import STKM
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
-from cv_stkm import generate_input_data
+from cv_stkm import generate_input_data, return_predicted_masked_image
 
 
 ### toy data
-timesteps = 10
-dimension = 3
-num_points = 100
-num_clusters = 2
+# timesteps = 10
+# dimension = 3
+# num_points = 100
+# num_clusters = 2
 
-data = np.random.rand(timesteps,dimension,num_points)
-weights = np.random.rand(timesteps,num_points,num_clusters)
-centers = np.random.rand(timesteps,dimension,num_clusters)
+# data = np.random.rand(timesteps, dimension, num_points)
+# weights = np.random.rand(timesteps, num_points, num_clusters)
+# centers = np.random.rand(timesteps, dimension, num_clusters)
 
-tkm = STKM(data = data)
-tkm.perform_clustering(num_clusters = 2, method = 'L1')
+# stkm = STKM(data=data)
+# stkm.perform_clustering(num_clusters=2, method="L1")
 
-plt.figure()
-plt.title('Error')
-plt.plot(tkm.err_hist)
+# plt.figure()
+# plt.title("Error")
+# plt.plot(tkm.err_hist)
 
-plt.figure()
-plt.title('Objective')
-plt.plot(tkm.obj_hist)
+# plt.figure()
+# plt.title("Objective")
+# plt.plot(tkm.obj_hist)
 
-# dir = 'cv_data/train/JPEGImages/0b34ec1d55/' #0ae1ff65a5/'
-# image_paths, input_data = generate_input_data(image_directory=dir)
+#######################################################################
+dir = "cv_data/plume/LeakVid54Frames/"
+image_paths, input_data = generate_input_data(image_directory=dir)
 
-# tkm = TKM(input_data)
-# tkm.perform_clustering_cosine(num_clusters = 2, lam = .8, max_iter = 1000)
+stkm = STKM(input_data[:100])
+stkm.perform_clustering(num_clusters=3, lam=0.8, max_iter=1000)
 
-
-# if __name__ == '__main__':
-#     cProfile.run('main()')
+return_predicted_masked_image(image_paths=image_paths, index=10, weights=stkm.weights)
